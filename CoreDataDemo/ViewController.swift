5c665644b2a0e316974bd9b6c7e71921a5ba8051
iOS-Pradeep <pradeep.sagar.ios@gmail.com>
2019-12-24 19:24:55 +0530

    hello

--- CoreDataDemo/ViewController.swift
+++ CoreDataDemo/ViewController.swift
@@ -7,12 +7,44 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        let entity = NSEntityDescription.entity(forEntityName: "Users", in: context)
+        let newUser = NSManagedObject(entity: entity!, insertInto: context)
+        newUser.setValue("Pradeep", forKey: "username")
+        newUser.setValue("abcd", forKey: "password")
+        newUser.setValue("32", forKey: "age")
+        
+        
+        // save the data
+        do {
+            try context.save()
+        } catch {
+            print("Failed Saving")
+        }
+        
+        // retrieve the data
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Users")
+        //request.predicate = NSPredicate(format: "age = %@", "12")
+        request.returnsObjectsAsFaults = false
+        do {
+            let result = try context.fetch(request)
+            for data in result as! [NSManagedObject] {
+               print(data.value(forKey: "username") as! String)
+          }
+            
+        } catch {
+            
+            print("Failed")
+        }
     }
 
 
